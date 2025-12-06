d858cb5e08fbf4eda1f56c679ec4ea2c86ea7cb5
JOSEPH MUNYAO MWILU <josephmunyao019@gmail.com>
2025-12-06 20:33:26 +0300

    initial commit

--- project_database/settings.py
+++ project_database/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6yw01)+lkdt)dkkbn-ejik*j3&cumzh#hdd!zvqx2v%hyg+(df
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,6 +128,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_DIRS = [BASE_DIR / 'sacco/static']
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = 'media/'
 
